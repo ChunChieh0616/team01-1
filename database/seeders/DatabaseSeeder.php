bc1afe062b501b97bb55d05ca970e31af037bc57
簡至昊 <a1357422@gmail.com>
2022-10-21 10:55:17 +0800

    1111021版本

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(StudentTableSeeder::class);
+        $this->call(BedTableSeeder::class);
+        $this->call(DormitoriesTableSeeder::class);
+        $this->call(SbrecordTableSeeder::class);
+        $this->call(LeaveTableSeeder::class);
+        $this->call(LateTableSeeder::class);
+        $this->call(RollcallsTableSeeder::class);
     }
 }
---
+++ database/seeders/DormitoriesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DormitoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
